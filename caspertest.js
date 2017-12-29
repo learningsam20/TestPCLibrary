d2b540ef96f2f724b6b3c4ee279d4a793dfdbad7
San <learningsam20@gmail.com>
2017-12-29 08:38:06 +0530

    Modified thenclick capture

--- caspertest.js
+++ caspertest.js
@@ -1,36 +1,27 @@
 var casper = require('casper').create();
 casper.start('http://www.ioffer.com/');
-var srchbtn = "test";
 
 casper.on("remote.message", function(msg){
     this.echo("remote.msg: " + msg);
 });
 
 casper.on("page.error", function(pageErr){
-    this.echo("page.err: " + JSON.stringify(pageErr));
+    this.echo("page err: " + JSON.stringify(pageErr));
 });
 
-function searchbtn(){
-casper.sendKeys("#search_query","phone")
-casper.echo(document.querySelector(".submit-button"))
-return ".submit-button > input"
-}
+casper.on("error", function(pageErr){
+    this.echo("casper err: " + pageErr);
+});
 
 casper.then(function() {
     casper.echo('Page title: ' + this.getTitle());
     casper.sendKeys("#search_query","phone");
-    //srchbtn = this.evaluate(searchbtn)
-    this.evaluate(function(){
-        this.echo(document.querySelector("input[type='submit']").getAttribute("onclick"))
-    })
-    
-    //casper.capture("test.png")
 });
 
-//casper.thenClick(srchbtn)
+casper.thenClick("input[type='submit']")
 
-// casper.thenOpen('http://phantomjs.org', function() {
-//     this.echo('Second Page: ' + this.getTitle());
-// });
+casper.then(function(){
+    casper.capture("test.png")
+})
 
 casper.run();
